c25bbddfda31644124ac0aa134fe0a1c6492dd5f
Davide <davide.zanella10@gmail.com>
2020-02-13 17:07:00 +0100

    Add sidebar menù

--- SchoolCalendar/SchoolCalendar/SchoolCalendar/settings.py
+++ SchoolCalendar/SchoolCalendar/SchoolCalendar/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
