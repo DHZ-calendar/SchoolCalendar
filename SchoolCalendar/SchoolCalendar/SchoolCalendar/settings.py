6b24c5b18baf37b746c080cec377c297dbf3e3d0
Davide <davide.zanella10@gmail.com>
2020-03-19 10:19:59 +0100

    Changed settings to connect to heroku's psql

--- SchoolCalendar/SchoolCalendar/SchoolCalendar/settings.py
+++ SchoolCalendar/SchoolCalendar/SchoolCalendar/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
