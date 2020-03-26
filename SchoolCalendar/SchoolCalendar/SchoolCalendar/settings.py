4f29d3a0fc085f50c150064ccf0c6872d2fe13cd
Davide <davide.zanella10@gmail.com>
2020-03-26 19:59:54 +0100

    Add language settings in settings.py

--- SchoolCalendar/SchoolCalendar/SchoolCalendar/settings.py
+++ SchoolCalendar/SchoolCalendar/SchoolCalendar/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -141,3 +142,11 @@ REST_FRAMEWORK = {
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, "locale"),
 )
+
+
+from django.utils.translation import gettext_lazy as _
+
+LANGUAGES = [
+    ('en', _('English')),
+    ('it', _('Italian')),
+]
