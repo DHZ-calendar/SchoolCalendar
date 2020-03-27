60d69bc50cdbcf378905aad3bd9dad257e2841c4
Davide <davide.zanella10@gmail.com>
2020-03-27 11:10:23 +0100

    Edit sidemenu to show only options for the user #9

---
+++ SchoolCalendar/SchoolCalendar/timetable/templatetags/is_adminschool.py
@@ -0,0 +1,8 @@
+from django import template
+from timetable import utils
+
+register = template.Library()
+
+@register.filter(name='is_adminschool')
+def is_adminschool(user):
+    return utils.is_adminschool(user)
