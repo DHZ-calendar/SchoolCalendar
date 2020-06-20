0bd27c4efb46b1286be0dea547d66c396b6beca1
Stefano Huber <stefano.huber@gmail.com>
2020-06-20 11:56:09 +0200

    Redirect from / to /timetable

--- SchoolCalendar/SchoolCalendar/SchoolCalendar/urls.py
+++ SchoolCalendar/SchoolCalendar/SchoolCalendar/urls.py
@@ -4,8 +4,10 @@ SchoolCalendar URL Configuration
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='user_redirect-view')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('timetable/', include('timetable.urls'))
