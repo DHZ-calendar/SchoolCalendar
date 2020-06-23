afbafde7a9f9164f5b49734581faa2018bd99024
Davide <davide.zanella10@gmail.com>
2020-06-23 11:52:32 +0200

    Fixed password reset #15

--- SchoolCalendar/SchoolCalendar/settings.py
+++ SchoolCalendar/SchoolCalendar/settings.py
@@ -150,3 +150,15 @@ LANGUAGES = [
     ('en', _('English')),
     ('it', _('Italian')),
 ]
+
+import json
+
+# Email backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+with open(os.path.join(BASE_DIR, "config.json")) as json_file:
+    config = json.load(json_file)
+    EMAIL_HOST = config['EMAIL_HOST']
+    EMAIL_PORT = config['EMAIL_PORT']
+    EMAIL_HOST_USER = config['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD = config['EMAIL_HOST_PASSWORD']
+    EMAIL_USE_TLS = config['EMAIL_USE_TLS']
