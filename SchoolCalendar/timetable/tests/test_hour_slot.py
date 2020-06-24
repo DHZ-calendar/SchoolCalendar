653d19a36c12e32b129dfe1a8d0318fa69799955
Stefano Huber <stefano.huber@gmail.com>
2020-06-24 10:00:11 +0200

    Add tests for hour slot

---
+++ SchoolCalendar/timetable/tests/test_hour_slot.py
@@ -0,0 +1,123 @@
+from django.test import TestCase
+from django.forms.models import model_to_dict
+from datetime import datetime, timedelta, time
+
+from timetable.models import *
+from timetable.forms import *
+from timetable.tests.base_test import BaseTestCase
+
+
+class HourSlotTestCase(BaseTestCase):
+    def setUp(self):
+        super(HourSlotTestCase, self).setUp()
+        self.school_year_2020 = SchoolYear(year_start=2020, date_start=datetime(month=8, year=2020, day=31))
+        self.school_year_2020.save()
+
+    def test_hour_slot_correct_creation(self):
+        pass
+        """
+        Test the creation of an HourSlot, using the correct admin and school.
+        :return:
+        """
+        data = {'hour_number': '4',
+                'starts_at': '11:05',
+                'ends_at': '12:00',
+                'school': self.s1,
+                'school_year': self.school_year_2020,
+                'day_of_week': '0',
+                'legal_duration_0': '1',   # Hour
+                'legal_duration_1': '0'}   # Minutes
+        f = HourSlotForm(user=self.a1, data=data)
+        f.full_clean()
+        self.assertTrue(f.is_valid(), msg='the errors are: {}'.format(f.errors))
+
+    def test_hour_slot_wrong_admin_for_school_creation(self):
+        """
+        Test the creation of a hour_slot, using the wrong school for the admin.
+        :return:
+        """
+        data = {'hour_number': '4',
+                'starts_at': '11:05',
+                'ends_at': '12:00',
+                'school': self.s1,      # Wrong school
+                'school_year': self.school_year_2020,
+                'day_of_week': '0',
+                'legal_duration_0': '1',
+                'legal_duration_1': '0'}
+        # But admin of school 2
+        f = HourSlotForm(user=self.a2, data=data)   # Admin is for different school
+        f.full_clean()
+        self.assertTrue(f.has_error('school'))   # The form should have an error on the school field.
+
+    def test_hour_slot_wrong_start_end_hour(self):
+        """
+        Test the creation of a wrong hour_slot, with starts_at > ends_at
+        :return:
+        """
+        data = {'hour_number': '4',
+                'starts_at': '13:05',    # Starts_at > ends_at
+                'ends_at': '12:00',
+                'school': self.s1,
+                'school_year': self.school_year_2020,
+                'day_of_week': '0',
+                'legal_duration_0': '1',
+                'legal_duration_1': '0'}
+        # But admin of school 1
+        f = HourSlotForm(user=self.a1, data=data)
+        f.full_clean()
+        self.assertFalse(f.is_valid())
+
+    def test_hour_slot_multiple_hour_number(self):
+        """
+        Test the creation of a wrong hour_slot, with multiple hour_number in the same day, school_year, school etc.
+        :return:
+        """
+        hs1 = HourSlot(hour_number=4,
+                       starts_at=time(hour=9, minute=0),
+                       ends_at=time(hour=10, minute=5),
+                       school=self.s1,
+                       school_year=self.school_year_2020,
+                       day_of_week=0,
+                       legal_duration=timedelta(seconds=3600))
+        hs1.save()
+        data = {'hour_number': '4',      # Same hour_number as above!
+                'starts_at': '11:05',
+                'ends_at': '12:00',
+                'school': self.s1,
+                'school_year': self.school_year_2020,
+                'day_of_week': '0',
+                'legal_duration_0': '1',
+                'legal_duration_1': '0'}
+        # But admin of school 1
+        f = HourSlotForm(user=self.a1, data=data)
+        f.full_clean()
+        hs1.delete()
+        self.assertFalse(f.is_valid())
+
+    def test_hour_slot_multiple_hour_number_but_different_days(self):
+        """
+        Test the creation of a hour_slot, with multiple hour_number in a different day, school_year, school etc.
+        :return:
+        """
+        hs1 = HourSlot(hour_number=4,
+                       starts_at=time(hour=9, minute=0),
+                       ends_at=time(hour=10, minute=5),
+                       school=self.s1,
+                       school_year=self.school_year_2020,
+                       day_of_week=1,
+                       legal_duration=timedelta(seconds=3600))
+        hs1.save()
+        data = {'hour_number': '4',      # Same hour_number as above!
+                'starts_at': '11:05',
+                'ends_at': '12:00',
+                'school': self.s1,
+                'school_year': self.school_year_2020,
+                'day_of_week': '0',        # But different day
+                'legal_duration_0': '1',
+                'legal_duration_1': '0'}
+        # But admin of school 1
+        f = HourSlotForm(user=self.a1, data=data)
+        f.full_clean()
+        hs1.delete()
+        self.assertTrue(f.is_valid(), msg=f.errors)
+
