2ed1bae9a69813ab0510e0e1f9c4fc5bfa397c0c
Stefano Huber <stefano.huber@gmail.com>
2020-06-24 09:59:53 +0200

    Solved bug in hour_slot form (school field may be null)

--- SchoolCalendar/timetable/forms.py
+++ SchoolCalendar/timetable/forms.py
@@ -244,23 +244,24 @@ class HourSlotForm(BaseFormWithSchoolCheck):
             self.add_error(None, forms.ValidationError(_('The start hour must be strictly smaller that the '
                                                          'end hour.')))
 
-        # Check if there is already the n-th hour of the day:
-        conflicting_hour_number = HourSlot.objects.filter(
-            school=self.cleaned_data['school'],
-            school_year=self.cleaned_data['school_year'],
-            day_of_week=self.cleaned_data['day_of_week'],
-            hour_number=self.cleaned_data['hour_number']
-        )
-        if conflicting_hour_number.exists():
-            if self.instance is not None and conflicting_hour_number.first().pk != self.instance.id:
-                self.add_error(None, forms.ValidationError(
-                    _("There is already the {}{} hour of the day "
-                      "for this school, day of week and school_year!".format(
-                                                            self.cleaned_data['hour_number'],
-                                                            _("th") if self.cleaned_data['hour_number'] % 10 > 3 else
-                                                            _("st") if self.cleaned_data['hour_number'] % 10 == 1 else
-                                                            _("nd") if self.cleaned_data['hour_number'] % 10 == 2 else
-                                                            _("rd")))))
+        if 'school' in self.cleaned_data:  # If school is not a key, clean_school has already failed.
+            # Check if there is already the n-th hour of the day:
+            conflicting_hour_number = HourSlot.objects.filter(
+                school=self.cleaned_data['school'],
+                school_year=self.cleaned_data['school_year'],
+                day_of_week=self.cleaned_data['day_of_week'],
+                hour_number=self.cleaned_data['hour_number']
+            )
+            if conflicting_hour_number.exists():
+                if self.instance is not None and conflicting_hour_number.first().pk != self.instance.id:
+                    self.add_error(None, forms.ValidationError(
+                        _("There is already the {}{} hour of the day "
+                          "for this school, day of week and school_year!".format(
+                                                                self.cleaned_data['hour_number'],
+                                                                _("th") if self.cleaned_data['hour_number'] % 10 > 3 else
+                                                                _("st") if self.cleaned_data['hour_number'] % 10 == 1 else
+                                                                _("nd") if self.cleaned_data['hour_number'] % 10 == 2 else
+                                                                _("rd")))))
 
         return self.cleaned_data
 
