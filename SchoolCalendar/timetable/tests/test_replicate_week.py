335bbdde15d12771d0733245a228dd7ecf224af3
Stefano Huber <stefano.huber@gmail.com>
2020-07-22 11:52:46 +0200

    Add test for rooms conflict on replicate week view.

--- SchoolCalendar/timetable/tests/test_replicate_week.py
+++ SchoolCalendar/timetable/tests/test_replicate_week.py
@@ -16,19 +16,23 @@ class ReplicateWeekTestCase(BaseTestCase):
         self.school_year_2020.save()
         # Create some teachers
         self.t1 = Teacher(username='t1', school=self.s1, email='t1@g.com', password='password_demo',
-                          first_name='fn', last_name='ln')
-        self.t2 = Teacher(username='t2', school=self.s1, email='t2@g.com')
-        self.t3 = Teacher(username='t3', school=self.s1, email='t3@g.com')
+                          first_name='fn1', last_name='ln1')
+        self.t2 = Teacher(username='t2', school=self.s1, email='t2@g.com', password='password_demo',
+                          first_name='fn2', last_name='ln2')
+        self.t3 = Teacher(username='t3', school=self.s1, email='t3@g.com', password='password_demo',
+                          first_name='fn3', last_name='ln3')
         self.t1.save()
         self.t2.save()
         self.t3.save()
         # Create some courses
         self.c1 = Course(year=1, section='A', school_year=self.school_year_2020, school=self.s1)
         self.c2 = Course(year=1, section='B', school_year=self.school_year_2020, school=self.s1)
+        self.c3 = Course(year=1, section='C', school_year=self.school_year_2020, school=self.s1)
         self.c1.save()
         self.c2.save()
+        self.c3.save()
         # Create some rooms
-        self.r1 = Room(name='lab1', capacity=2, school=self.s1)
+        self.r1 = Room(name='lab1', capacity=2, school=self.s1)   # The room has capacity 2.
         self.r1.save()
         # Create some subjects
         self.sub1 = Subject(name='Maths', school=self.s1)
@@ -66,10 +70,18 @@ class ReplicateWeekTestCase(BaseTestCase):
                                            hours=100,
                                            hours_bes=100,
                                            hours_co_teaching=100)
+        self.h3_3 = HoursPerTeacherInClass(teacher=self.t3,
+                                           course=self.c3,
+                                           subject=self.sub3,
+                                           school=self.s1,
+                                           hours=100,
+                                           hours_bes=100,
+                                           hours_co_teaching=100)
         self.h1_1.save()
         self.h1_2.save()
         self.h2_2.save()
         self.h3_2.save()
+        self.h3_3.save()
 
         # Fill one week with some assignments that are going to be replicated.
         self.ass1 = Assignment(teacher=self.t1,
@@ -128,3 +140,155 @@ class ReplicateWeekTestCase(BaseTestCase):
         self.assertTrue(len(json_res['room_conflicts']) == 0)
         self.assertTrue(len(json_res['course_conflicts']) == 0)
         ass2.delete()
+
+    def test_check_conflict_3(self):
+        """
+        Try to replicate one week which should be with a course conflict.
+        """
+        ass2 = Assignment(teacher=self.t2,
+                          course=self.c1,    # In course 1, hence a course conflict
+                          subject=self.sub1,
+                          school=self.s1,
+                          room=self.r1,
+                          date=datetime(year=2020, month=5, day=11),  # Monday 11/5/2020
+                          hour_start=time(hour=9, minute=0),
+                          hour_end=time(hour=10, minute=0),
+                          bes=False,
+                          co_teaching=False,
+                          substitution=False,
+                          absent=False,
+                          free_substitution=False)
+        ass2.save()
+        response = self.c.post('/timetable/check_week_replication/2020-05-04/2020-05-24',
+                               {'assignments[]': [self.ass1.id]})
+        json_res = response.json()
+        self.assertTrue(len(json_res['teacher_conflicts']) == 0)
+        self.assertTrue(len(json_res['room_conflicts']) == 0)
+        self.assertTrue(len(json_res['course_conflicts']) == 1)
+        ass2.delete()
+
+    def test_check_conflict_4(self):
+        """
+        Try to replicate one week where a room is filled with 2 courses, but the room has capacity 2.
+        """
+        ass2 = Assignment(teacher=self.t2,
+                          course=self.c2,    # In course 1, hence a course conflict
+                          subject=self.sub2,
+                          school=self.s1,
+                          room=self.r1,
+                          date=datetime(year=2020, month=5, day=11),  # Monday 11/5/2020
+                          hour_start=time(hour=9, minute=0),
+                          hour_end=time(hour=10, minute=0),
+                          bes=False,
+                          co_teaching=False,
+                          substitution=False,
+                          absent=False,
+                          free_substitution=False)
+        ass2.save()
+        response = self.c.post('/timetable/check_week_replication/2020-05-04/2020-05-24',
+                               {'assignments[]': [self.ass1.id]})
+        json_res = response.json()
+        self.assertTrue(len(json_res['teacher_conflicts']) == 0)
+        self.assertTrue(len(json_res['room_conflicts']) == 0)
+        self.assertTrue(len(json_res['course_conflicts']) == 0)
+        ass2.delete()
+
+    def test_check_conflict_5(self):
+        """
+        Try to replicate one week where a room is filled with 3 courses, but the room has capacity 2.
+        """
+        ass2 = Assignment(teacher=self.t2,
+                          course=self.c2,
+                          subject=self.sub2,
+                          school=self.s1,
+                          room=self.r1,            # Room 1 is already filled with 2 courses.
+                          date=datetime(year=2020, month=5, day=11),  # Monday 11/5/2020
+                          hour_start=time(hour=9, minute=0),
+                          hour_end=time(hour=10, minute=0),
+                          bes=False,
+                          co_teaching=False,
+                          substitution=False,
+                          absent=False,
+                          free_substitution=False)
+        ass2.save()
+        ass3 = Assignment(teacher=self.t3,
+                          course=self.c3,
+                          subject=self.sub3,
+                          school=self.s1,
+                          room=self.r1,           # room 1 is filled with 2 courses already
+                          date=datetime(year=2020, month=5, day=11),  # Monday 11/5/2020
+                          hour_start=time(hour=9, minute=0),
+                          hour_end=time(hour=10, minute=0),
+                          bes=False,
+                          co_teaching=False,
+                          substitution=False,
+                          absent=False,
+                          free_substitution=False)
+        ass3.save()
+        # This has no use for the room conflict, but could still cause troubles for the view,
+        # so it is better to test with it.
+        ass4 = Assignment(teacher=self.t3,
+                          course=self.c3,
+                          subject=self.sub3,
+                          school=self.s1,
+                          room=self.r1,           # room 1 is filled with 2 courses already
+                          date=datetime(year=2020, month=5, day=18),  # Monday 18/5/2020
+                          hour_start=time(hour=9, minute=0),
+                          hour_end=time(hour=10, minute=0),
+                          bes=False,
+                          co_teaching=False,
+                          substitution=False,
+                          absent=False,
+                          free_substitution=False)
+        ass4.save()
+        response = self.c.post('/timetable/check_week_replication/2020-05-04/2020-05-24',
+                               {'assignments[]': [self.ass1.id]})
+        json_res = response.json()
+        self.assertTrue(len(json_res['teacher_conflicts']) == 0)
+        self.assertTrue(len(json_res['room_conflicts']) == 2)  # There are 2 possible courses that are in conflict!
+        self.assertTrue(len(json_res['course_conflicts']) == 0)
+        ass2.delete()
+        ass3.delete()
+        ass4.delete()
+
+    def test_check_conflict_6(self):
+        """
+        Try to replicate one week where a room is filled with 3 courses, on Monday but of different days.
+        """
+        ass2 = Assignment(teacher=self.t2,
+                          course=self.c2,
+                          subject=self.sub2,
+                          school=self.s1,
+                          room=self.r1,
+                          date=datetime(year=2020, month=5, day=11),  # Monday 11/5/2020
+                          hour_start=time(hour=9, minute=0),
+                          hour_end=time(hour=10, minute=0),
+                          bes=False,
+                          co_teaching=False,
+                          substitution=False,
+                          absent=False,
+                          free_substitution=False)
+        ass2.save()
+        ass3 = Assignment(teacher=self.t2,
+                          course=self.c2,
+                          subject=self.sub2,
+                          school=self.s1,
+                          room=self.r1,
+                          date=datetime(year=2020, month=5, day=18),  # Monday 18/5/2020
+                          hour_start=time(hour=9, minute=0),
+                          hour_end=time(hour=10, minute=0),
+                          bes=False,
+                          co_teaching=False,
+                          substitution=False,
+                          absent=False,
+                          free_substitution=False)
+        ass3.save()
+        response = self.c.post('/timetable/check_week_replication/2020-05-04/2020-05-24',
+                               {'assignments[]': [self.ass1.id]})
+        json_res = response.json()
+        self.assertTrue(len(json_res['teacher_conflicts']) == 0)
+        # There should not be any room conflict, assignments are on different days
+        self.assertTrue(len(json_res['room_conflicts']) == 0)
+        self.assertTrue(len(json_res['course_conflicts']) == 0)
+        ass2.delete()
+        ass3.delete()
